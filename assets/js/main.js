a622c5847975a474ad5cf82fabee742db9fa1e98
Yazdan Zinati <yazdan@tuta.io>
2021-09-11 23:45:47 -0400

    Add timeline

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,8 +35,25 @@ navigationLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContent = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
 
+        tabContent.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 /*==================== SERVICES MODAL ====================*/
 
 
