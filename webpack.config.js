4bd76e37d833e67eda3bd87dadc0db1f498050c4
Yazdan Zinati <contact@yazdan.me>
2022-04-19 10:13:11 -0400

    Add light/dark mode to the redesign.

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,14 @@ module.exports = {
         test: /\.(s(a|c)ss)$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
     ],
   },
   plugins: [
