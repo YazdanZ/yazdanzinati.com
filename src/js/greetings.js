ee481bc7f2c7c770f30793a7f2d26fc93988982e
Yazdan Zinati <contact@yazdan.me>
2022-04-22 17:08:11 -0400

    Add js and styling to new website.

---
+++ src/js/greetings.js
@@ -0,0 +1,50 @@
+/**
+ * Greets the visitor with a random hello.
+ */
+function greet(firstVisit) {
+  const greetingText = document.getElementById("greeting-text");
+  const greetings = [
+    "Hello",
+    "Hi",
+    "Howdy",
+    "Bonjour",
+    "Hiya",
+    "Greetings",
+    "Howdy-do",
+    "Cheers",
+    "Hey",
+    "Heyyy",
+    "Ahoy",
+    "Ciao",
+    "G’day",
+    "Heya",
+  ];
+
+  let greeting;
+  if (firstVisit) {
+    greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  } else {
+    greeting = "Hey, you again";
+  }
+  greetingText.innerHTML = greeting;
+}
+
+/**
+ * Waves the hand emoji.
+ */
+function wave() {
+  const greetingWave = document.getElementById("greeting-wave");
+  greetingWave.classList.add("wave"); // add animation class to element
+}
+
+/**
+ * On page load, greet with a message depending on if
+ * this is the first visit with a wave animation.
+ */
+if (document.cookie == "HasVisited=true") {
+  greet(false);
+} else {
+  document.cookie = "HasVisited=true";
+  greet(true);
+}
+wave();
