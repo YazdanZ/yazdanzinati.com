4523c188996d66cccb0ed2b46802bfbbde6aa4db
Yazdan Zinati <contact@yazdan.me>
2022-04-30 00:42:28 -0400

    Change the theme selector to a slider.

--- src/js/switch_theme.js
+++ src/js/switch_theme.js
@@ -1,16 +1,12 @@
-const themeButton = document.getElementById("theme-button");
+const themeButton = document.getElementById("themeswitcher");
 const darkTheme = "dark-theme";
-const iconTheme = "uil-sun";
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () =>
   document.body.classList.contains(darkTheme) ? "dark" : "light";
-const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
@@ -18,17 +14,12 @@ if (selectedTheme) {
   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
     darkTheme
   );
-  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
-    iconTheme
-  );
 }
 
 // Activate/deactivate the theme manually with the button
 themeButton.addEventListener("click", () => {
   // Add or remove the dark / icon theme
   document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
   // We save the theme and the current icon that the user chose
   localStorage.setItem("selected-theme", getCurrentTheme());
-  localStorage.setItem("selected-icon", getCurrentIcon());
 });
